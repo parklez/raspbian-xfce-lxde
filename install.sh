ce00f82252fed39b2590a418c112d28994098a34
parklez <29131182+parklez@users.noreply.github.com>
2017-10-15 20:44:41 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,14 +10,17 @@ sudo apt-get install --no-install-recommends xserver-xorg -y
 # 2. (A) LXDE GUI:
 #sudo apt-get install lxde-core lxappearance -y
 # 2. (B) XFCE GUI:
-sudo apt-get install xfce4 xfce4-terminal -y
-sudo apt-get install gvfs-backends
+#sudo apt-get install xfce4 xfce4-terminal -y
+#sudo apt-get install gvfs-backends
+# 2. (C) PIXEL GUI:
+sudo apt-get install --no-install-recommends raspberrypi-ui-mods lxterminal gvfs
 
 # 3. Login Manager:
-sudo apt-get install lightdm -y
+#sudo apt-get install lightdm -y
+#PIXEL GUI needs no lightdm
 
-# Remove xscreensaver:
-sudo apt-get remove --auto-remove xscreensaver -y
+# Remove xscreensaver(?):
+#sudo apt-get remove --auto-remove xscreensaver -y
 
 # The "add-apt-get-repository" command:
 sudo apt-get install software-properties-common python-software-properties -y
@@ -42,22 +45,12 @@ sudo apt-get install p7zip-full -y
 sudo apt-get install leafpad -y
 
 # Python-Related:
-sudo apt-get install python3 -y
 sudo apt-get install idle3 -y
 sudo apt-get install python3-pip -y
 sudo apt-get install python-tk -y
-#sudo apt-get install python-pip -y
 
-# Github:
-#sudo apt-get install git -y
-
-# Chromium Browser / rpi-youtube:
-#git clone https://github.com/kusti8/RPi-chromium
-#cd RPi-chromium
-#sudo bash ./install.sh
-
-#Firefox/Iceweasel browser then install yourself "NoScript" Addon: (it's shit)
-#sudo apt-get install iceweasel -y
+# Chromium Browser
+sudo apt-get install chromium-browser -y
 
 # Torrent client:
 #sudo apt-get install transmission -y
